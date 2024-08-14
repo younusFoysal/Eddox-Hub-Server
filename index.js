b95674a7ec9db71cf537fa8861565237ee518c5b
younusFoysal <younus.faysal@gmail.com>
2024-08-14 21:46:33 +0600

    MongoDB setup Done

--- index.js
+++ index.js
@@ -1,3 +1,82 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+const port = process.env.PORT || 5000
+
+
+const corsOptions = {
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'https://eddoxhub.web.app',
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token
+    console.log(token)
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            console.log(err)
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded
+        next()
+    })
+}
+
+// mongo db url
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3baw43.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+})
+
+async function run() {
+    try {
+        const db = client.db('eddoxhubDB')
+        const usersCollection = db.collection('users')
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db('admin').command({ ping: 1 })
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        )
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Eddox Hub Server is Running...')
+})
+
+
+app.listen(port, () => {
+    console.log(`Eddox Hub Server is Running on port ${port}`)
+})
